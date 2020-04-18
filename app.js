0408686f719a9318b65342528354afcc21f78a98
Hugo Magalhães <hello@hugomagalhaes.design>
2020-04-18 16:49:23 +0200

    Finished Wall ball project

--- app.js
+++ app.js
@@ -1,63 +1,77 @@
-let positionX
-let positionY
-let speedX 
-let speedY
+// Movement variables
+let position;
+let speed;
 
 // Ball radius
-let radius
+let radius;
 
 // Animation sound clip
-let sound
+let sound;
 
+// Preloads sound
 function preload() {
-    sound = loadSound("drop.mp3")
+  sound = loadSound("drop.mp3");
 }
 
-
+// Creates animation setup
 function setup() {
-    // Sets up a canvas spanning the entire window
-    createCanvas(windowWidth, windowHeight)
+  // Sets up a canvas spanning the entire window
+  createCanvas(windowWidth, windowHeight);
 
-    positionX = 100
-    positionY = 50
+  // Adds values to variables
+  position = createVector(100, 50);
+  speed = createVector(5, 5);
 
-    speedX = 5
-    speedY = 5
-
-    radius = 50
+  // Sets circle radius
+  radius = 50;
 }
 
 function draw() {
-    // Sets canvas background
-    background("#FFB6C155")
+  // Sets canvas background
+  background("#FFB6C155");
 
-    // Circle properties
-    fill("white")
-    noStroke()
+  // Circle properties
+  fill("white");
+  noStroke();
 
-    // Draws circle
-    circle(positionX,positionY, radius * 2)
+  // Draws circle
+  circle(position.x, position.y, radius * 2);
 
-    // Animates circle
-    positionX = positionX + speedX
-    positionY = positionY + speedY
+  // Animates circle
+  // ↳ Adds speedX to PositionX on each frame
+  // positionX = positionX + speedX
+  // ↳ Adds speedY to positionY on each frame
+  // positionY = positionY + speedY
 
-    // Reverses position when touching the egdes of the window
-    if (positionX > windowWidth - radius || positionX < radius) {
-        speedX = speedX * -1
-        sound.play()
-    }
-    if (positionY > windowHeight - radius || positionY < radius) {
-        speedY = speedY * -1
-        sound.play()
-    }
+  // ↳ Adds speed to position object
+  position.add(speed);
 
-    // Modulates positionX between left edge (radius) and right edge (windowWidth - radius)
-    positionX = constrain(positionX, radius, windowWidth - radius)
-    positionY = constrain(positionY, radius, windowHeight - radius)
+  // Slows down ball
+  speed.mult(0.999)
+
+  // Reverses position and plays a sound when touching the horizontal egdes of the window
+  if (position.x > windowWidth - radius || position.x < radius) {
+    speed.x = speed.x * -1;
+    sound.play();
+  }
+  // Reverses position and plays a sound when touching the horizontal egdes of the window.
+  if (position.y > windowHeight - radius || position.y < radius) {
+    speed.y = speed.y * -1;
+    sound.play();
+  }
+
+  // Modulates the position of ball to be between the left (radius) and right edge (windowWidth - radius). The ball won't disappear when resizing the browser window.
+  position.x = constrain(position.x, radius, windowWidth - radius);
+  position.y = constrain(position.y, radius, windowHeight - radius);
 }
 
-// On windowResize redraws canvas
+function mouseClicked() {
+    // Picks a number between 0 and TWO PI
+    let angle = random(TWO_PI)
+    speed.rotate(angle)
+}
+
+// Automatically resizes canvas on windowResize
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
+  resizeCanvas(windowWidth, windowHeight);
 }
