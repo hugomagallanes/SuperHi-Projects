abc0ca075b6fd760c11aa33e9581dd095dead958
Hugo Magalhães <hello@hugomagalhaes.design>
2020-04-19 19:32:04 +0200

    Hue-changing brsuh

--- 02 Flower Brush/app.js
+++ 02 Flower Brush/app.js
@@ -1,16 +1,56 @@
+
+// Stores flowers
+let flowers
+
+// Sets brush's hue
+let hue 
+
+// Sets brush's size
+let size
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  flowers = []
+  hue = random(0,100)
+  size = 20
 }
 
+// Continously redraws frame
 function draw() {
-  // background("#111118");
 
-  // circle(100, 50, 50)
-  // fill("orange");
-  // noStroke()
+  // ↳ Sets background color
+  background("#111118");
+
+  // ↳ Draws flower based on flowers array
+  flowers.forEach( flower => {
+    flower.draw()
+  })
+
+
 }
 
-function mouseClicked() {
-  // Creates new instance of Flower
-  new Flower(mouseX, mouseY)
+function mouseDragged() {
+
+  // Increases variable value on mouseDrag
+  hue = hue + .1
+  size = size + 1
+
+  // Constrains brush size 20-100
+  size = constrain(size, 20, 100)
+
+  // Brush resets to blue at HSB 100
+  if (hue > 100) {
+    hue = 0
+  }
+
+  // Creates new instance of Flower and pushes it to flowers
+  flowers.push(new Flower(mouseX, mouseY, hue, size))
+}
+
+
+function mouseReleased() {
+  hue = random(0,100)
+  size = 20
 }
--- 02 Flower Brush/flower.js
+++ 02 Flower Brush/flower.js
@@ -1,9 +1,25 @@
 // Creates Flower class
 class Flower {
-    constructor(x, y) {
-        // Creates square
-        noStroke();
-        fill("red");
-        square(x - 25, y - 25, 50);
-    }
+  constructor(x, y, hue, size) {
+    // Assign parameters to variables x and y
+    this.x = x;
+    this.y = y;
+    this.hue = hue;
+    this.midSize = size;
+    // this.alpha = 100
+  }
+
+  draw() {
+    colorMode(HSB, 100);
+    noStroke();
+
+
+    // fill(this.hue, 100, 100, this.alpha);
+
+    fill(this.hue, 100, 100)
+    circle(this.x, this.y, this.midSize);
+
+    // Fade draw each time this function rans
+    // this.alpha = this.alpha - 1
+  }
 }
